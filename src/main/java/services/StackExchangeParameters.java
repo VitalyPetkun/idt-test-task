4dc6c21feb6c65ae12a85bdd377e54c6709cbc0a
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-10-05 13:11:33 +0300

    Add file StackExchangeParameters.java

---
+++ src/main/java/services/StackExchangeParameters.java
@@ -0,0 +1,21 @@
+package services;
+
+public enum StackExchangeParameters {
+
+    SITE("site="),
+    PAGE("page="),
+    PAGE_SIZE("pagesize="),
+    ORDER("order="),
+    SORT("sort="),
+    FILTER("filter=");
+
+    private String parameter;
+
+    StackExchangeParameters(String parameter) {
+        this.parameter = parameter;
+    }
+
+    public String getParameter(String value) {
+        return parameter.concat(value).concat("&");
+    }
+}
