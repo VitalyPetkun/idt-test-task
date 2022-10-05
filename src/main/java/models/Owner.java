8f6d87839f5fcb76f21c4843213e80fb775d7722
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-10-05 13:58:06 +0300

    Add file Owner.java

---
+++ src/main/java/models/Owner.java
@@ -0,0 +1,14 @@
+package models;
+
+import lombok.Data;
+
+@Data
+public class Owner {
+
+    private String reputation;
+    private String user_id;
+    private String user_type;
+    private String profile_image;
+    private String display_name;
+    private String link;
+}
