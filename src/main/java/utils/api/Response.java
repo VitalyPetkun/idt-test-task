da5e1674a91b6c54324f26475353d05f7563917c
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-10-05 08:39:20 +0300

    Add file Response.java

---
+++ src/main/java/utils/api/Response.java
@@ -0,0 +1,22 @@
+package utils.api;
+
+import io.restassured.response.ValidatableResponse;
+
+public class Response {
+
+    private int status;
+    private String body;
+
+    protected Response(ValidatableResponse validatableResponse) {
+        status = validatableResponse.extract().statusCode();
+        body = validatableResponse.extract().body().asString();
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
