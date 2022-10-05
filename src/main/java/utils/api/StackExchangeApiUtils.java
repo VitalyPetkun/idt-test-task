9b4c8152da54737c5b593fac09a656bb2718e655
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-10-05 16:32:40 +0300

    Add file AnswersTest.java

--- src/main/java/utils/api/StackExchangeApiUtils.java
+++ src/main/java/utils/api/StackExchangeApiUtils.java
@@ -11,10 +11,11 @@ public class StackExchangeApiUtils extends ApiUtils{
         super();
     }
 
-    public static Response getAnswers(String site, String page, String pageSize, String order, String sort, String filter) {
+    public static Response getAnswers(String site, String page, String pageSize,
+                                      String order, String sort, String filter, ContentType contentType) {
         return doGet(VERSION.getVersion().concat(ANSWERS.getPoint()).
                 concat(SITE.getParameter(site)).concat(PAGE.getParameter(page)).
                 concat(PAGE_SIZE.getParameter(pageSize)).concat(ORDER.getParameter(order)).
-                concat(SORT.getParameter(sort)).concat(FILTER.getParameter(filter)), ContentType.JSON);
+                concat(SORT.getParameter(sort)).concat(FILTER.getParameter(filter)), contentType);
     }
 }
---
+++ src/test/java/tests/AnswersTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import io.restassured.http.ContentType;
+import models.Answer;
+import models.Owner;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utils.JsonConverter;
+import utils.SmartLogger;
+import utils.api.Response;
+import utils.api.StackExchangeApiUtils;
+
+public class AnswersTest extends BaseTest {
+
+    private Response response;
+
+    @DataProvider
+    public Object[][] getParameters() {
+        return new Object[][]{
+                {"stackoverflow", "1", "10", "desc", "activity", "default"}
+        };
+    }
+
+    @Test(dataProvider = "getParameters")
+    public void getAnswers(String site, String page, String pageSize, String order, String sort, String filter) {
+        SmartLogger.logStep(1, "Get request for %s owners".concat(pageSize));
+        response = StackExchangeApiUtils.getAnswers(site, page, pageSize, order, sort, filter, ContentType.JSON);
+        Answer answer = JsonConverter.getObject(response.getBody(), Answer.class);
+        Assert.assertEquals(response.getStatus(), HttpStatus.SC_OK, "Don't correct status code");
+        Assert.assertEquals(answer.getItems().size(), Integer.parseInt(pageSize), "Don't correct items size");
+        answer.getItems().stream().forEach(Item -> Assert.assertTrue(Item.getOwner().getClass().equals(Owner.class),
+                        "Not all items of array have Owner"));
+        answer.getItems().stream().forEach(Item -> Assert.assertTrue(Item.getOwner().getLink().
+                        contains(Item.getOwner().getUser_id()), "Not all Owners links formed with user_id"));
+        answer.getItems().stream().forEach(Item -> Assert.assertTrue(Item.getOwner().getLink().
+                        contains(Item.getOwner().getDisplay_name().toLowerCase().replaceAll(" ", "-")),
+                "Not all Owners links formed with display_name"));
+    }
+}
