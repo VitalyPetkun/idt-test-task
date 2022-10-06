009f3b5c3bf1b2a4b9ec6ec153ceaf167311e0b6
Vitaly Petkun <p.u.d.i.n.g.69.v@gmail.com>
2022-10-06 09:56:02 +0300

    Add checking response for not null

--- src/test/java/tests/AnswersTest.java
+++ src/test/java/tests/AnswersTest.java
@@ -12,9 +12,9 @@ import utils.SmartLogger;
 import utils.api.Response;
 import utils.api.StackExchangeApiUtils;
 
-public class AnswersTest extends BaseTest {
+import java.util.Arrays;
 
-    private Response response;
+public class AnswersTest extends BaseTest {
 
     @DataProvider
     public Object[][] getParameters() {
@@ -25,20 +25,33 @@ public class AnswersTest extends BaseTest {
 
     @Test(dataProvider = "getParameters")
     public void getAnswers(String site, String page, String pageSize, String order, String sort, String filter) {
-        SmartLogger.logStep(1, "Get request for %s owners".concat(pageSize));
-        response = StackExchangeApiUtils.getAnswers(site, page, pageSize, order, sort, filter, ContentType.JSON);
+        SmartLogger.logStep(1, String.format("Get request for %s owners", pageSize));
+        Response response = StackExchangeApiUtils.getAnswers(site, page, pageSize, order, sort, filter, ContentType.JSON);
+        Answer answer = JsonConverter.getObject(response.getBody(), Answer.class);
         Assert.assertEquals(response.getStatus(), HttpStatus.SC_OK, "Don't correct status code");
 
-        Answer answer = JsonConverter.getObject(response.getBody(), Answer.class);
+        SmartLogger.logInfo("Checking response for not null");
+        Assert.assertNotNull(answer, "Answer is null");
+        Arrays.stream(answer.getClass().getFields()).forEach(field ->
+                Assert.assertNotNull(field, "Answer field is null"));
+        answer.getItems().forEach(Item -> Arrays.stream(Item.getClass().getFields()).forEach(field ->
+                Assert.assertNotNull(field, "Item field is null")));
+        answer.getItems().forEach(Item -> Arrays.stream(Item.getOwner().getClass().getFields()).forEach(field ->
+                Assert.assertNotNull(field, "Owner field is null")));
+
+        SmartLogger.logInfo("Checking correct items size");
         Assert.assertEquals(answer.getItems().size(), Integer.parseInt(pageSize), "Don't correct items size");
 
-        answer.getItems().stream().forEach(Item -> Assert.assertTrue(Item.getOwner().getClass().equals(Owner.class),
+        SmartLogger.logInfo("Checking all items of array for have Owner");
+        answer.getItems().forEach(Item -> Assert.assertEquals(Item.getOwner().getClass(), Owner.class,
                 "Not all items of array have Owner"));
 
-        answer.getItems().stream().forEach(Item -> Assert.assertTrue(Item.getOwner().getLink().
+        SmartLogger.logInfo("Checking all Owners links for formed with user_id");
+        answer.getItems().forEach(Item -> Assert.assertTrue(Item.getOwner().getLink().
                 contains(Item.getOwner().getUser_id()), "Not all Owners links formed with user_id"));
 
-        answer.getItems().stream().forEach(Item -> Assert.assertTrue(Item.getOwner().getLink().
+        SmartLogger.logInfo("Checking all Owners links for formed with display_name");
+        answer.getItems().forEach(Item -> Assert.assertTrue(Item.getOwner().getLink().
                         contains(Item.getOwner().getDisplay_name().toLowerCase().replaceAll("[ .]", "-")),
                 "Not all Owners links formed with display_name"));
     }
